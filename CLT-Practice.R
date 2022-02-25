5d9f6d17eff444814af0486f707d1669f8778e09
Hemant Panchal <96882837+HemantPanchal14@users.noreply.github.com>
2022-02-25 23:20:25 +0530

    Update CLT-Practice.R

--- CLT-Practice.R
+++ CLT-Practice.R
@@ -1,17 +1,15 @@
 # LEARNING CENTRAL LIMIT THEOREM
-# CLT is Sampling distribution of Mean
 
-#case study-1 : Mercury makes a 2.4lt v-6 engine,the laser Xri used in speedboat
-# the companies engineer believe that the engine delivers an average power of 220
-#horse power and that the SD of power delivered is 15 uorsepower.
-#A potential buyer intends to sample 100 engines ( each engine to be run a single time)
-# we have to find out what is the probability that the SAMPLE MEAN will be less than the 217 horsepower
-# Now, from above , we know Mean=220,SD=15 , N=100
-# we have to calculate SD of sample size i.e. sigma/sqrt(n)= 1.5
+#case study-1: Mercury makes a 2.4lt v-6 engine, the laser Xri used in a speedboat the companies engineer believe that the engine delivers an average power of 220 horse power and that the SD of power delivered is 15 horsepower.
+#a potential buyer intends to sample 100 engines (each engine to be run a single time),We have to find out what is the probability that the SAMPLE MEAN will be less than the 217 horsepower
+Now, from above, we know Mean=220, SD=15, N=100, 
+We have to calculate SD of sample size i.e.CLT= sigma/sqrt(n)= 1.5
 
-# will calculate probability
+# will calculate the probability
 
 pnorm(217,220,1.5)
+
+i.e. 0.02275013
 
 # Case study-2: During any hour in large department store, the average number of shopper
 # is 448 with the standard deviation of 21 shoppers what is the probability
@@ -22,3 +20,5 @@ pnorm(217,220,1.5)
 # as per CLT = SD= 3
 
 pnorm(446,448,3)-pnorm(441,448,3)
+
+i.e. 0.2426772
